c498c917988c862cb80ac234d91667b477793160
unknown <patrickkeita80@yahoo.com>
2023-06-21 03:18:40 -0700

    Update person.rb

--- person.rb
+++ person.rb
@@ -1,5 +1,10 @@
-class Person
+require_relative 'nameable'
+require_relative 'capitalize_decorator'
+require_relative 'trimmer_decorator'
+
+class Person < Nameable
   def initialize(age, name, parent_permision = `true`)
+    super()
     @id = generate_unique_id
     @age = age
     @name = name
@@ -22,4 +27,15 @@ class Person
   def generate_unique_id
     rand(1..1000)
   end
+
+  def correct_name
+    @name
+  end
 end
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+capitalizedPerson = CapitalizeDecorator.new(person)
+puts capitalizedPerson.correct_name
+capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+puts capitalizedTrimmedPerson.correct_name
