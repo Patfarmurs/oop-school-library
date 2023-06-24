795c54a49628c67b46e8b9527c78a3c0dd03babf
unknown <patrickkeita80@yahoo.com>
2023-06-23 22:03:07 -0700

    Update main.rb

--- main.rb
+++ main.rb
@@ -5,20 +5,33 @@ class Main
     @app = App.new
   end
 
-  puts 'Welcome to School Library App!'
   # rubocop:disable Metrics/CyclomaticComplexity
-  def run_option
+  def menu
+    puts 'Welcome to School Library App!'
     loop do
-      display_menu_options
+      puts 'Please choose an option by entering a number:'
+      puts '1 - List all books'
+      puts '2 - List all people'
+      puts '3 - Create a person'
+      puts '4 - Create a book'
+      puts '5 - Create a rental'
+      puts '6 - List all rentals for a given person id'
+      puts '7 - Exit'
       option = gets.chomp
 
       case option
-      when '1' then list_all_books
-      when '2' then list_all_people
-      when '3' then create_person
-      when '4' then create_book
-      when '5' then create_rental
-      when '6' then list_rentals
+      when '1'
+        @app.list_all_books
+      when '2'
+        @app.list_all_people
+      when '3'
+        @app.create_person
+      when '4'
+        @app.create_book
+      when '5'
+        @app.create_rental
+      when '6'
+        @app.list_rentals
       when '7'
         puts 'Thank you for using our library ¯\^-^/¯'
         return
@@ -27,43 +40,8 @@ class Main
       end
     end
   end
-
   # rubocop:enable Metrics/CyclomaticComplexity
-  def display_menu_options
-    puts 'Please choose an option by entering a number:'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person id'
-    puts '7 - Exit'
-  end
-
-  def list_all_books
-    @app.list_all_books
-  end
-
-  def list_all_people
-    @app.list_all_people
-  end
-
-  def create_person
-    @app.create_person
-  end
-
-  def create_book
-    @app.create_book
-  end
-
-  def create_rental
-    @app.create_rental
-  end
-
-  def list_rentals
-    @app.list_rentals
-  end
 end
 
 main = Main.new
-main.run_option
+main.menu
